e7b4d79ae866422b1e76f4b52cc47935b9bb0453
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-08-17 03:44:14 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,16 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
-);
+CREATE TABLE animals
+(
+    id integer NOT NULL,
+    name character varying(100) NOT NULL,
+    date_of_birth date NOT NULL,
+    escape_attempts integer DEFAULT 0,
+    neutered boolean NOT NULL,
+    weight_kg numeric
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS animals
+    OWNER to postgres;
