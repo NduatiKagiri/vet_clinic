bfe70843aabc444d9038ef08878fd02ed9ea35fe
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-08-17 19:55:43 +0300

    Correct Boolean values

--- data.sql
+++ data.sql
@@ -1,13 +1,13 @@
 /* Populate database with sample data. */
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('1', 'Agumon', '2020-2-3', '0', '1', '10.23');
+VALUES ('1', 'Agumon', '2020-2-3', '0', 'true', '10.23');
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('2', 'Gabumon', '2018-11-15', '2', '1', '8');
+VALUES ('2', 'Gabumon', '2018-11-15', '2', 'true', '8');
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('3', 'Pikachu', '2021-1-7', '1', '0', '15.04');
+VALUES ('3', 'Pikachu', '2021-1-7', '1', 'false', '15.04');
 
 INSERT INTO animals(id, name, date_of_birth, escape_attempts, neutered, weight_kg)
-VALUES ('4', 'Devimon', '2017-5-12', '5', '1', '11');
+VALUES ('4', 'Devimon', '2017-5-12', '5', 'true', '11');
