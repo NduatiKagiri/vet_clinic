6440ade24552e6f8b6043d02319b4da4793175cd
NduatiKagiri <87394385+NduatiKagiri@users.noreply.github.com>
2022-08-18 19:28:35 +0300

    Create new tables and alter animals

--- schema.sql
+++ schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE animals
     escape_attempts integer DEFAULT 0,
     neutered boolean NOT NULL,
     weight_kg numeric
-)
+);
 
 TABLESPACE pg_default;
 
@@ -17,4 +17,27 @@ ALTER TABLE IF EXISTS animals
 
 -- Day 2. Query and Update Animals Table
 ALTER TABLE animals
-  ADD species character varying(200);
+    ADD species character varying(200);
+
+-- Day 3. Query multiple tables
+CREATE TABLE owners
+(
+    id integer NOT NULL PRIMARY KEY,
+    full_name character varying(200) NOT NULL,
+    age integer
+);
+
+CREATE TABLE species
+(
+    id integer NOT NULL PRIMARY KEY,
+    full_name character varying(200) NOT NULL
+);
+
+ALTER TABLE animals
+    DROP COLUMN species,
+    ADD species_id integer,
+    ADD owner_id integer;
+
+ALTER TABLE animals
+    ADD CONSTRAINT fk_animal_species FOREIGN KEY (species_id) REFERENCES species (id),
+    ADD CONSTRAINT fk_animal_owner FOREIGN KEY (owner_id) REFERENCES owners (id);
